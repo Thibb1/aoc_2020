0b0bce890a0baebe9ea10e6beec0650ec2e53b30
VOLTE <thibault2.brumaire@epitech.eu>
2020-12-22 15:31:24 +0100

    ➕ day01

---
+++ day01/day01.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use Data::Dumper;
+
+(do print "$0: provide file name as argument\n"), (do exit 84) if ($#ARGV + 1 < 1);
+
+my $filename = $ARGV[0];
+
+my @numbers;
+my @answers;
+
+open(FH, '<', $filename) or die $!;
+
+while (<FH>) { push @numbers, $_; }
+for my $i (@numbers) { for my $j (@numbers) { push @answers, $i, $j if ($i + $j == 2020); }}
+print "===Matching===\n";
+print Dumper \@answers;
+printf "Answer = %d\n", $answers[0] * $answers[1];
